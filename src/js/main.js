9747e7da244d4766fd833a336906ac55a52c059f
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-01-25 10:54:21 -0500

    carga de la logica

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,108 @@
 import '../scss/style.scss'
 import * as bootstrap from 'bootstrap'
+
+const URLBASE = "http://localhost:3000"
+const nameUser = document.getElementById("user-name")
+const userAge = document.getElementById("user-age")
+const userId = document.getElementById("user-id")
+const form = document.getElementById("form")
+const tbody = document.getElementById("tbody")
+let userCache
+
+document.addEventListener("DOMContentLoaded", () => {
+    getUsers()
+})
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    if (userCache===undefined) {
+        saveUser()
+    } else {
+        updateUser(userCache)
+    }
+    
+})
+
+tbody.addEventListener('click', (event) => {
+    if (event.target.classList.contains("btn-primary")) {
+        userCache = event.target.getAttribute("data-id")
+        nameUser.value=event.target.parentElement.parentElement.getElementsByTagName('td')[0].textContent;
+        userAge.value=event.target.parentElement.parentElement.getElementsByTagName('td')[1].textContent;
+    }
+})
+
+async function saveUser() {
+    const user = {
+        name: nameUser.value,
+        age: userAge.value
+    }
+
+    const response = await fetch(`${URLBASE}/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: nameUser.value,
+            age: userAge.value,
+        })
+    })
+
+    const data = await response.json()
+    renderUsers(data)
+}
+
+async function updateUser(id) {
+    const user = {
+        name: nameUser.value,
+        age: userAge.value
+    }
+
+    const response = await fetch(`${URLBASE}/users/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+
+    userCache=undefined
+}
+
+async function getUsers() {
+    const response = await fetch(`${URLBASE}/users`)
+    const data = await response.json()
+    renderUsers(data)
+}
+
+async function deleteUser(id) {
+    const response = await fetch(`${URLBASE}/users/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function renderUsers(data) {
+    cleanTbody()
+    data.forEach((element, index) => {
+        tbody.innerHTML += `
+        <tr>
+            <th scope="row">${index + 1}</th>
+            <td>${element.name}</td>
+            <td>${element.age}</td>
+            <td>
+                <button type="button" class="btn btn-primary" data-id="${element.id}">Edit</button>
+                <button type="button" class="btn btn-danger" onclick="eliminar('${element.id}')">Delete</button>
+            </td>
+        </tr>
+        `
+    });
+}
+
+function cleanTbody() {
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild)
+    }
+}
