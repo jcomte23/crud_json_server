d75fbe6c75909fe3e6189da1f842c81a9696b0d5
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-01-26 12:04:49 -0500

    the code was improved

--- src/js/login_register.js
+++ src/js/login_register.js
@@ -1,2 +1,16 @@
 import '../scss/login_register.scss'
 import * as bootstrap from 'bootstrap'
+
+const form = document.getElementById("form")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const passwordConfirm = document.getElementById("password-confirm")
+
+form.addEventListener("submit",(event) => {
+  event.preventDefault()
+  registerUser()
+})
+
+function registerUser() {
+    
+}
