3e919fbd18f539c6db9faf80278a4468a6eed173
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-01-26 12:13:17 -0500

    the code was improved

--- src/js/login_register.js
+++ src/js/login_register.js
@@ -6,11 +6,22 @@ const email = document.getElementById("email")
 const password = document.getElementById("password")
 const passwordConfirm = document.getElementById("password-confirm")
 
-form.addEventListener("submit",(event) => {
-  event.preventDefault()
-  registerUser()
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    registerUser()
 })
 
 function registerUser() {
-    
+    const { validated, message } = validatePassword()
+}
+
+function validatePassword() {
+    if (password.value != passwordConfirm.value) {
+        return {
+            validated: false,
+            message: "the passwords do not match"
+        }
+    }
+
+    return { validated: true }
 }
