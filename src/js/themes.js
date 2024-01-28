ed16338c06dec43f58b96d1c0447e1d4955cd8fa
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-01-28 15:11:15 -0500

    the settings for to change the theme were created

---
+++ src/js/themes.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {  
+    const theme=localStorage.getItem("jws_theme")
+    if (theme===null) {
+        document.documentElement.setAttribute('data-bs-theme', "light")
+    }else{
+        document.documentElement.setAttribute('data-bs-theme', theme)
+    }
+
+    const btnLight = document.getElementById('theme-light')
+    const btnDark = document.getElementById('theme-dark')
+
+    btnLight.addEventListener("click",() => {
+        document.documentElement.setAttribute('data-bs-theme', "light");
+        localStorage.setItem('jws_theme', "light");
+    })
+
+    btnDark.addEventListener("click",(event) => {
+        document.documentElement.setAttribute('data-bs-theme', "dark");
+        localStorage.setItem('jws_theme', "dark");
+    })
+
+});
