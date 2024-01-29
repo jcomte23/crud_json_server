12748c141bc30cf61c37498cb8ee56a2d803fc6e
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-01-29 13:06:29 -0500

    mejoras en el codigo

--- src/js/login.js
+++ src/js/login.js
@@ -4,58 +4,103 @@ import * as bootstrap from 'bootstrap'
 import { createDropdownTheme } from '../components/dropdown_theme'
 
 createDropdownTheme()
+const URLSERVER = "http://localhost:3000"
 const form = document.getElementById("form")
 const email = document.getElementById("email")
 const password = document.getElementById("password")
 
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
     if (!form.checkValidity()) {
         event.preventDefault()
     } else {
         event.preventDefault()
-        loginUser()
+        const { validatedEmail, messageEmail } = await validateEmailInDatabase()
+        if (validatedEmail && (messageEmail === password.value)) {
+            loginUser()
+        } else {
+            if (validatedEmail === false) {
+                Swal.fire({
+                    toast: true,
+                    position: "top-end",
+                    icon: "error",
+                    title: `${messageEmail}`,
+                    showConfirmButton: false,
+                    timer: 2000,
+                    timerProgressBar: true,
+                });
+            } else {
+                Swal.fire({
+                    toast: true,
+                    position: "top-end",
+                    icon: "error",
+                    title: `incorrect password`,
+                    showConfirmButton: false,
+                    timer: 2000,
+                    timerProgressBar: true,
+                });
+            }
+
+        }
+
     }
 })
 
 
-function loginUser() {
-
-    switch (password.value) {
-        case "1":
-            Swal.fire({
-                toast: true,
-                position: "top-end",
-                icon: "success",
-                title: `Success`,
-                showConfirmButton: false,
-                timer: 2000,
-                timerProgressBar: true,
-            });
-            break;
-        case "2":
-            Swal.fire({
-                toast: true,
-                position: "top-end",
-                icon: "error",
-                title: `incorrect password`,
-                showConfirmButton: false,
-                timer: 2000,
-                timerProgressBar: true,
-            });
-        case "3":
-            Swal.fire({
-                toast: true,
-                position: "top-end",
-                icon: "error",
-                title: `User was not found`,
-                showConfirmButton: false,
-                timer: 2000,
-                timerProgressBar: true,
-            });
-            break;
-
-        default:
-            break;
+async function validateEmailInDatabase() {
+    const response = await fetch(`${URLSERVER}/users?email=${email.value}`)
+    const data = await response.json()
+    if (data.length === 1) {
+        return {
+            validatedEmail: true,
+            messageEmail: data[0].password
+        }
     }
+    return {
+        validatedEmail: false,
+        messageEmail: "the email does not exist"
+    }
+
+
+    // switch (password.value) {
+    //     case "1":
+    //         Swal.fire({
+    //             toast: true,
+    //             position: "top-end",
+    //             icon: "success",
+    //             title: `Success`,
+    //             showConfirmButton: false,
+    //             timer: 2000,
+    //             timerProgressBar: true,
+    //         });
+    //         break;
+    //     case "2":
+    //         Swal.fire({
+    //             toast: true,
+    //             position: "top-end",
+    //             icon: "error",
+    //             title: `incorrect password`,
+    //             showConfirmButton: false,
+    //             timer: 2000,
+    //             timerProgressBar: true,
+    //         });
+    //     case "3":
+    //         Swal.fire({
+    //             toast: true,
+    //             position: "top-end",
+    //             icon: "error",
+    //             title: `User was not found`,
+    //             showConfirmButton: false,
+    //             timer: 2000,
+    //             timerProgressBar: true,
+    //         });
+    //         break;
+
+    //     default:
+    //         break;
+    // }
+}
+
+function loginUser() {
+    window.location.href="src/users/index.html"
 }
